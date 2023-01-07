671e37d8f3193288cd57ccc98a75af47e81c0975
iamjames5 <lennox.james@student.nhlstenden.com>
2023-01-07 13:07:35 +0100

    Code for the Homepage

---
+++ app/public/homepage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<title>Homepage</title>
+</head>
+<body id="gridContainer">
+	<div id="main">
+		<div  class="christmasContainer">
+			<div class="welcomeBox">De Morgenster,  welcome!</div>
+			<div class="christmas">
+				<h3>Christmas Event</h3>
+				<p> 21.12.2022</p>
+				<p>10:00-15:00</p>
+			</div>
+		</div> 
+		<div class="schoolKid"> 
+			<img class="img" src="img/school 1.png" alt="Boy in class">  
+		</div>
+		<div class="eventBox">
+			<a href="#" class="event">
+				<div>
+					Event
+				</div>
+			</a>
+			<a href="#" class="login">
+				<div>
+					Login
+				</div>
+			</a>
+			<div class="placeHolder">
+			</div>
+		</div>
+		<div class="emptyBox">
+		</div>
+   </div>
+</body>
+</html>
