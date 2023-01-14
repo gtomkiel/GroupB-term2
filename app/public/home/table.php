9763eae37008b0f0b74cb46d3299540bc39bf538
SaraRodrigues1 <sara.rodrigues.cardoso@student.nhlstenden.com>
2023-01-14 14:05:04 +0100

    backend addition for events page

--- app/public/home/table.php
+++ app/public/home/table.php
@@ -1,47 +1,47 @@
+<?php
+
+require_once('../src/db/connect.php');
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link type="text/css" rel="stylesheet" href="../src/styles/style.css" />
-      <title></title>
+      <title>List of events</title>
 
 </head>
 <body>
-
 	<div id="gridContainer">
-	
+		<?php require_once('../src/utils/header.php') ?>
 		<div class="info2">
 				<ul>	
-					<li>Events:</li>
+					<li>List of events</li>
+					<li><a href="/home/">Go back</a></li>
 				</ul>
 		</div>
-
 		<table id="Events">
 			<tr>
 				<th>Event Name</th>
 				<th>Event day</th>
 			</tr>
-			<tr>
-				<td></td>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td></td>
-			</tr>
-		</table>
+			<?php
+			
+			$stmt = $db->prepare('SELECT * FROM Events');
+			$stmt->execute();
+			$events = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+			foreach ($events as $event) {
+				echo "	<tr>
+							<th>".$event['name']."</th>
+							<th>".$event['date']."</th>
+						</tr>";
+			}
+			
+			?>
+		</table>
 	</div>
-	
+	<?php require_once('../src/utils/footer.php') ?>
 </body>
 </html>
-
-
