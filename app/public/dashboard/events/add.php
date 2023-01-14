61176debb2b88d379ac632cb3cb72fbf1d21fea3
Clarissa003 <clarissa.dobircianu@student.nhlstenden.com>
2023-01-14 12:13:37 +0100

    add event backend completed

--- app/public/dashboard/events/add.php
+++ app/public/dashboard/events/add.php
@@ -1,18 +1,76 @@
+<?php
+
+require_once('../../src/db/connect.php');
+
+session_start();
+
+if(!isset($_SESSION['ID'])) {
+    header('Location: /login/');
+    exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link type="text/css" rel="stylesheet" href="src/styles/style.css">
-    <title>Add Event</title>
+	<title>Events</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="/src/styles/style.css">
 </head>
 <body id="gridContainer">
-   <div class="formInput">
-   <form action="login.html" method="POST">
-	<input type="text" name="name" id="name" placeholder="Event Name">
-	<input type="text" name="date" id="date" placeholder="Event Date">
-   <input type="submit" name="Add Event" id="addEvent" value="Add Event">
+	<? require_once('../../src/utils/header.php'); ?>
+	<div class="index">
+		<div class="welcome">
+         <b>Event insertion</b>
+		</div>
+	</div>
+	<form action="#" method="POST">
+		<div class="formInput">
+			<input type="text" name="name" id="name" placeholder="Event name">
+			<input type="text" name="date" id="date" placeholder="Date">
+			<input type="submit" name="Add Absence" id="addAbsence" value="Add Event">
+			<div class="button2">
+				<a href="index.php">Back</a>
+			</div>
+		</div>
 	</form>
-   </div>
-   <? require_once('footer.php'); ?>
+	<? require_once('../../src/utils/footer.php'); ?>
 </body>
 </html>
+
+<? 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+   $name = filter_input(INPUT_POST,"name");
+   $date = filter_input(INPUT_POST,"date");
+
+   if(empty($name)) {
+      $err[]="Please enter your secondname<br>";
+   }
+
+   if(empty($date)) {
+      $err[]="Please enter your secondname<br>";
+   }
+
+   if(count($err)>0) {
+      foreach($err as $error){
+         echo "$error<br>";
+      }
+   }
+
+   try {
+      $event = $db->prepare('INSERT INTO Events(name, date) VALUES (:name, :date)');
+      $event->bindParam(':name',$name,PDO::PARAM_STR);
+      $event->bindParam(':date',$date,PDO::PARAM_STR);
+      $event->execute();
+   } catch (Exception $e) {
+      echo $e;
+   }	
+}
+
+require_once('../../src/utils/footer.php'); 
+
+?>
